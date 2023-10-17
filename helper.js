368532e0dd2e8dc899d05640e4d03885b03c736b
fatemzh <abid.fatem@hotmail.fr>
2023-10-17 13:44:40 +0200

    section6 finie

--- helper.js
+++ helper.js
@@ -1,3 +1,12 @@
 exports.success = (message, data) => {
     return{message,data}
 }
+
+exports.getUniqueId = (pokemons) => {
+    //Transforme tableau de pokémon en un tableau d'id avec la fonction map
+    const pokemonsIds =pokemons.map(pokemon => pokemon.id)
+    //Identifie le plus grand identifiant, reduce compare les élémnents du tableau 
+    const maxId = pokemonsIds.reduce((a,b) => Math.max(a+b))
+    const uniqueId = maxId + 1
+    return uniqueId
+}
