509fb233a69bfd6bc7d73b093ddee92eacde3b19
fatemzh <abid.fatem@hotmail.fr>
2023-10-18 11:29:36 +0200

    Partie10 finie

--- src/models/pokemon.js
+++ src/models/pokemon.js
@@ -1,4 +1,7 @@
 /* L’API Rest et la Base de données : Créer un modèle Sequelize */
+
+const validTypes = ['Plante', 'Poison', 'Feu', 'Eau', 'Insecte', 'Vol', 'Normal', 'Electrik', 'Fée']
+
 module.exports = (sequelize, DataTypes) => {
     return sequelize.define('Pokemon', {
       id: {
@@ -8,23 +11,64 @@ module.exports = (sequelize, DataTypes) => {
       },
       name: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        unique: {message: "Ce nom est déjà pris."},
+        validate: {
+          notEmpty: {message: "Veuillez remplir le champ nom."},
+          notNull: {message: "Le nom est une propriété requise."},
+          min: {args: [1], message: "Le nom doit contenir au moins une lettre."},
+          max: {args: [25], message: "Le nom ne peut contenir plus de 25 lettres."}
+        }
       },
       hp: {
         type: DataTypes.INTEGER,
-        allowNull: false
+        allowNull: false, 
+        validate: {
+          isInt: {message : "Veuillez saisir uniquement des nombres entiers pour les points de vie."},
+          notNull: {message: "Les points de vie sont une propriété requise."},
+          min: {args: [0], message: "Les points de vie doivent être inclus entre 0 et 999."},
+          max: {args: [999], message: "Les points de vie doivent être inclus entre 0 et 999."},
+        }
       },
       cp: {
         type: DataTypes.INTEGER,
-        allowNull: false
+        allowNull: false,
+        validate: {
+          isInt: {message : "Veuillez saisir uniquement des nombres entiers pour les points d'attaque."},
+          notNull: {message: "Les points de combat sont une propriété requise."},
+          min: {args: [0], message: "Les points de vie doivent être inclus entre 0 et 99."},
+          max: {args: [99], message: "Les points de vie doivent être inclus entre 0 et 99."},
+        }
       },
       picture: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        validate: {
+          isUrl: {message : "Veuillez entrer une URL uniquement."},
+          notNull: {message: "L'URL d'une image est requise."}
+        }
       },
       types: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          isTypesValid(value)
+          {
+            if(!value)
+            {
+              throw new Error ("Un pokémon doit avoir au moins un type.")
+            }
+            if(value.split(",").length > 3)
+            {
+              throw new Error ("Un pokémon ne peut pas avoir plus de 3 types.")
+            }
+            value.split(",").forEach(type =>{
+              if(!validTypes.includes(type)){
+                throw new Error (`Le type d'un pokémon doit appartenir à la liste suivante ${validTypes}`)
+              }
+            })
+          }
+        },
         get(){
           return this.getDataValue('types').split(',')
         },
--- src/routes/updatePokemon.js
+++ src/routes/updatePokemon.js
@@ -1,5 +1,6 @@
 const { Pokemon } = require('../db/sequelize')
-  
+const {ValidationError} = require('sequelize')
+
 module.exports = (app) => {
   app.put('/api/pokemons/:id', (req, res) => {
     const id = req.params.id
@@ -17,7 +18,14 @@ module.exports = (app) => {
       })
     })
     .catch(error => {
-      const message = 'Le pokémon n\'a pas pu être moidifié. Réessayez dans quelques instants.'
+      if(error instanceof ValidationError) {
+        return res.status(400).json({ message: error.message, data: error });
+      }
+      if(error instanceof UniqueConstraintError)
+      {
+        return res.status(400).json({message : error.message, data: error})
+      }
+      const message = 'Le pokémon n\'a pas pu être modifié. Réessayez dans quelques instants.'
       res.status(500).json({message, data:error})
     })
   })
