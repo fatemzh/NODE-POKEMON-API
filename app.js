60f6fd2d8735e0714a4c52d94022311c480b1588
fatemzh <abid.fatem@hotmail.fr>
2023-10-25 19:51:49 +0200

    port dynamique pour le déploiement

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const favicon = require('serve-favicon')
 const bodyParser = require('body-parser')
 const sequelize = require('./src/db/sequelize')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app
   .use(favicon(__dirname + '/favicon.ico'))
