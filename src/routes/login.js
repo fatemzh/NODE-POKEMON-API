2d8190d6696ddfce4b89a8669ceeea7130b2272e
fatemzh <abid.fatem@hotmail.fr>
2023-11-04 10:31:34 +0100

    cors not done

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,38 +1,52 @@
-/* Authentification : Créer un modèle User avec Sequelize */
+// Importation du modèle User depuis la configuration Sequelize
 const { User } = require('../db/sequelize')
+
+// Importation de la bibliothèque bcrypt pour la vérification des mots de passe
 const bcrypt = require('bcrypt')
+
+// Importation de la bibliothèque jwt pour la création de tokens d'authentification
 const jwt = require('jsonwebtoken')
+
+// Importation de la clé privée pour la signature des tokens JWT
 const privateKey = require('../auth/private_key')
 
+// Exportation d'une fonction qui prend en paramètre l'application Express
 module.exports = (app) => {
+  // Définition d'une route POST pour la connexion des utilisateurs
   app.post('/api/login', (req, res) => {
   
+    // Recherche de l'utilisateur par son nom d'utilisateur
     User.findOne({ where: { username: req.body.username } }).then(user => {
 
+        // Si l'utilisateur n'est pas trouvé, renvoie un message d'erreur
         if(!user)
         {
             const message = 'L\'utilisateur demandé n\'existe pas.' 
             return res.status(404).json({message})
         }
 
+      // Vérification du mot de passe avec bcrypt
       bcrypt.compare(req.body.password, user.password).then(isPasswordValid => {
+        // Si le mot de passe n'est pas valide, renvoie un message d'erreur
         if(!isPasswordValid) {
           const message = `Le mot de passe est incorrect.`;
           return res.status(404).json({ message})
         }
 
-        // JWT 
+        // Si le mot de passe est valide, création d'un token JWT
         const token = jwt.sign(
             {userId : user.id},
             privateKey,
             {expiresIn: '1000h'}
         )
 
+        // Renvoie un message de succès avec le token JWT
         const message = `L'utilisateur a été connecté avec succès`;
         return res.json({ message, data: user, token})
       })
     })
     .catch(error =>{
+        // Gestion des erreurs lors de la connexion
         const message = 'Connexion échouée, veuillez réessayer plus tard.'
         return res.json({message, data: error})
     })
--- src/routes/updatePokemon.js
+++ src/routes/updatePokemon.js
@@ -1,32 +1,47 @@
+// Importation du modèle Pokemon depuis la configuration Sequelize
 const { Pokemon } = require('../db/sequelize')
-const {ValidationError} = require('sequelize')
+
+// Importation de l'erreur de validation de Sequelize
+const { ValidationError } = require('sequelize')
+
+// Importation du middleware d'authentification
 const auth = require('../auth/auth')
 
+// Exportation d'une fonction qui prend en paramètre l'application Express
 module.exports = (app) => {
+  // Définition d'une route PUT pour mettre à jour un Pokémon par son identifiant
   app.put('/api/pokemons/:id', auth, (req, res) => {
+    // Récupération de l'identifiant du Pokémon depuis les paramètres de la route
     const id = req.params.id
 
+    // Mise à jour du Pokémon dans la base de données
     Pokemon.update(req.body, {
       where: { id: id }
     })
     .then(_ => {
+      // Après la mise à jour, recherche du Pokémon par son identifiant pour renvoyer les données à jour
       return Pokemon.findByPk(id).then(pokemon => {
+        // Si le Pokémon n'est pas trouvé, renvoie un message d'erreur
         if(pokemon === null){
           const message = "Le pokémon demandé n'existe pas, veuillez réessayer avec un autre identifiant."
           return res.status(404).json({message})
         }
+        // Si le Pokémon est trouvé, renvoie un message de succès avec les données à jour
         const message = `Le pokémon ${pokemon.name} a bien été modifié.`
         res.json({message, data: pokemon })
       })
     })
     .catch(error => {
+      // Gestion des erreurs de validation
       if(error instanceof ValidationError) {
         return res.status(400).json({ message: error.message, data: error });
       }
+      // Gestion des erreurs de contrainte unique
       if(error instanceof UniqueConstraintError)
       {
         return res.status(400).json({message : error.message, data: error})
       }
+      // Gestion des autres erreurs
       const message = 'Le pokémon n\'a pas pu être modifié. Réessayez dans quelques instants.'
       res.status(500).json({message, data:error})
     })
